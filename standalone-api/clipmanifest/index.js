b6074cde4e654d2848b63efc89a6a22ad26c3af9
Osmar Bento da Silva Junior <osmarb@amazon.com>
2023-02-23 23:54:59 -0500

    input error validation on the clipmanifest API and standalone clipmanifest API, 500ms improvement on the clipmanifest API

--- standalone-api/clipmanifest/index.js
+++ standalone-api/clipmanifest/index.js
@@ -7,16 +7,70 @@ const s3Bucket = process.env.STORAGE_IVSRECORDINGS_BUCKETNAME
 
 exports.handler = async (event) => {
   // (1) parse event body and get the start and end time of the clip
-  const body = JSON.parse(event.body)
-  const excutionTime = Date.now()
-  const startTime = body.start_time
-  const endTime = body.end_time
-  const byteRange = body.byte_range
-  let masterURL = url.parse(body.master_url).pathname
+  const {
+    start_time: startTime,
+    end_time: endTime,
+    master_url: urlMaster,
+    byte_range: byteRange
+  } = JSON.parse(event.body)
+
+  // validate input fields
+  validateRequiredFields({ startTime, endTime, urlMaster, byteRange })
+  validateEndTime({ startTime, endTime })
+  validateNumericFields({ startTime, endTime })
+
+  const executionTime = Date.now()
+  const masterURL = url.parse(urlMaster).pathname
   const pathName = masterURL.replace('/master.m3u8', '')
   const s3BucketFolder = s3Bucket + pathName
+  const excutionTime = Date.now()
 
-  let genericExt = []
+  // validation functions
+  function validateRequiredFields({
+    startTime,
+    endTime,
+    urlMaster,
+    byteRange
+  }) {
+    if (
+      !startTime ||
+      !endTime ||
+      !urlMaster ||
+      byteRange === undefined ||
+      byteRange === null ||
+      typeof byteRange !== 'boolean'
+    ) {
+      throw Object.assign(
+        new Error(
+          'start_time, end_time, master_url, and byte_range are required.'
+        ),
+        { statusCode: 400 }
+      )
+    }
+  }
+
+  function validateEndTime({ startTime, endTime }) {
+    if (endTime <= startTime || endTime === 0) {
+      throw Object.assign(
+        new Error('end_time must be greater than start_time.'),
+        {
+          statusCode: 400
+        }
+      )
+    }
+  }
+
+  function validateNumericFields({ startTime, endTime }) {
+    if (isNaN(startTime) || isNaN(endTime)) {
+      throw Object.assign(
+        new Error('start_time and end_time must be numbers.'),
+        {
+          statusCode: 400
+        }
+      )
+    }
+  }
+
   // (2) request the master manifest from S3 and return the raw manifest
   const rawMasterManifest = await getManifestfromS3(
     byteRange
@@ -24,18 +78,30 @@ exports.handler = async (event) => {
       : masterURL
   )
 
+  validateByteRange(rawMasterManifest, byteRange)
+
+  function validateByteRange(rawMasterManifest, byteRange) {
+    if (rawMasterManifest === 404 && byteRange === true) {
+      throw Object.assign(
+        new Error('This stream does not support byte range manifest'),
+        { statusCode: 404 }
+      )
+    }
+    return null
+  }
+
   // (3) parse the master manifest and return the media playlists
-  function parseMaster(masterFile) {
-    let lines = masterFile.split('\n')
-    let playlists = []
-    for (let i = 0; i < lines.length; i++) {
-      let line = lines[i].trim()
-      const regExp = new RegExp('^[0-9]{1,3}p[0-9]{2}')
+  function parseMaster(masterManifest) {
+    const playlists = []
+    const regExp = /^[0-9]{1,3}p[0-9]{2}/
+    for (const playlistLine of masterManifest.split('\n')) {
+      const line = playlistLine.trim()
       if (regExp.test(line)) {
-        let playlist = {}
-        playlist.uri = line
-        playlist.base = line.split('/')[0]
-        playlist.filename = line.split('/')[1]
+        const playlist = {
+          uri: line,
+          base: line.split('/')[0],
+          filename: line.split('/')[1]
+        }
         playlists.push(playlist)
       }
     }
@@ -57,17 +123,20 @@ exports.handler = async (event) => {
   const rawPlaylistManifest = await getPlaylistManifest(mediaPlaylists)
 
   // (5) parse the playlist manifest and return the segments
+  let genericExt = []
   function parsePlaylistwithPDT(playlist) {
     let lines = playlist.split('\n')
     let segments = []
     // add manifest generic lines
-    genericExt = lines.filter(
-      (line) =>
-        line.startsWith('#EXT-X-TARGETDURATION') |
-        line.startsWith('#ID3-EQUIV-TDTG') |
-        line.startsWith('#EXT-X-PLAYLIST-TYPE') |
-        line.startsWith('#EXT-X-MEDIA-SEQUENCE') |
-        line.startsWith('#EXT-X-TWITCH-ELAPSED-SECS')
+    const filterConditions = [
+      '#EXT-X-TARGETDURATION',
+      '#ID3-EQUIV-TDTG',
+      '#EXT-X-PLAYLIST-TYPE',
+      '#EXT-X-MEDIA-SEQUENCE',
+      '#EXT-X-TWITCH-ELAPSED-SECS'
+    ]
+    genericExt = lines.filter((line) =>
+      filterConditions.some((condition) => line.startsWith(condition))
     )
     // add the media playlist files
     for (let i = 0; i < lines.length; i++) {
@@ -110,25 +179,19 @@ exports.handler = async (event) => {
 
   // (7) create the playlist manifest for the clip
   function createPlaylistManifest(segments) {
+    const totalDuration = segments.reduce(
+      (total, segment) => total + segment.duration,
+      0
+    )
     let playlist = `#EXTM3U\n#EXT-X-VERSION:4\n`
-    let duration = 0
-    for (let i = 0; i < segments.length; i++) {
-      let segment = segments[i]
-      duration += segment.duration
-    }
-    //playlist += `#EXT-X-TARGETDURATION:${Math.ceil(duration)}\n`
-
     for (let i = 0; i < genericExt.length; i++) {
       playlist += `${genericExt[i]}\n`
     }
-    playlist += `#EXT-X-TWITCH-TOTAL-SECS:${Math.ceil(duration)}\n`
+    playlist += `#EXT-X-TWITCH-TOTAL-SECS:${totalDuration.toFixed(3)}\n`
     for (let i = 0; i < segments.length; i++) {
-      let segment = segments[i]
-      if (segment.byte) {
-        playlist += `#EXT-X-PROGRAM-DATE-TIME:${segment.pdt}\n#EXTINF:${segment.duration}\n${segment.byte}\n${segment.uri}\n`
-      } else {
-        playlist += `#EXT-X-PROGRAM-DATE-TIME:${segment.pdt}\n#EXTINF:${segment.duration}\n${segment.uri}\n`
-      }
+      const segment = segments[i]
+      const byteTag = segment.byte ? `${segment.byte}\n` : ''
+      playlist += `#EXT-X-PROGRAM-DATE-TIME:${segment.pdt}\n#EXTINF:${segment.duration}\n${byteTag}${segment.uri}\n`
     }
     playlist += '#EXT-X-ENDLIST'
     return playlist
@@ -139,29 +202,36 @@ exports.handler = async (event) => {
   )
 
   // (8) write to S3 the filtered Master Manifest
+  let writePromises = []
+
+  // (8) write to S3 the filtered Master Manifest
   if (rawMasterManifest) {
     let fileName = new RegExp(mediaPlaylists[0].filename, 'g')
     let newMaster = rawMasterManifest
       .toString()
       .replace(fileName, `${excutionTime}_clip_playlist.m3u8`)
-    await writeToS3(
+    const masterWritePromise = writeToS3(
       newMaster,
       `${excutionTime}_clip_master.m3u8`,
       s3BucketFolder
     )
+    writePromises.push(masterWritePromise)
   }
 
   // (9) write to S3 the filtered Media Playlist
   if (mediaPlaylists) {
-    for await (const playlist of mediaPlaylists) {
+    for (const playlist of mediaPlaylists) {
       const newLocation = `${s3BucketFolder}/${playlist.base}`
-      await writeToS3(
+      const playlistWritePromise = writeToS3(
         newPlaylist,
         `${excutionTime}_clip_playlist.m3u8`,
         newLocation
       )
+      writePromises.push(playlistWritePromise)
     }
   }
+
+  await Promise.all(writePromises)
 
   // (10) return the response
   const responseBody = [
@@ -195,6 +265,9 @@ async function getManifestfromS3(key) {
     const response = await s3.getObject(params).promise()
     return response.Body.toString('ascii')
   } catch (error) {
+    if (error.statusCode === 404) {
+      return 404
+    }
     console.error(error)
     throw error
   }
