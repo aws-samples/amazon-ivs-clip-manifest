b789731fcf69dc2759adf28bf6f6b3961dbe42e4
Boris Kauffmann <boriskff@amazon.com>
2023-03-02 13:19:27 -0300

    fixing CameCase

---
+++ manifest-clip-ui/src/components/apis/getRecordings.js
@@ -0,0 +1,18 @@
+import { GET_RECORDING_API } from '../../config'
+
+export function getRecordingsAPI() {
+  console.log('get Recordings', GET_RECORDING_API)
+  const url = GET_RECORDING_API
+
+  return fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      Accept: 'application/json'
+    })
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error', error)
+      return error
+    })
+}
